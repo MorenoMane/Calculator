b941357ddc75088a7c002f1cd0ad98d4f4b56136
Jacobo <isjacobo@gmail.com>
2023-10-17 23:16:47 -0500

    first commit

---
+++ Program.fs
@@ -0,0 +1,82 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+
+//recursion and match 
+let readOperation () =
+    printfn "What operation do you wish to accomplish "
+    
+    let input = Console.ReadLine()
+    
+    input
+
+let isValidInteger (input: string) =
+    match Int32.TryParse(input) with
+    | (true, _) -> true  // Parsing successful
+    | _ -> false        // Parsing failed
+
+let rec readOperand () =
+    let input = Console.ReadLine()
+
+    match isValidInteger input with
+    |true -> Int32.Parse(input)
+    |false -> 
+        printfn "Invalid input . Try again" 
+        readOperand()
+   
+
+let performCalculation() =
+    let operation = readOperation()
+
+    printfn "Enter first Operand"
+    let operand1 = readOperand()
+    printfn "Enter Second Operand"
+    let operand2 = readOperand()
+
+    printf "Calculating %d" operand1
+    printf " %s"operation 
+    printfn " %d"operand2
+
+    match operation with
+    |"+" -> operand1 + operand2
+    |"-" -> operand1 - operand2
+    |"*" -> operand1 * operand2
+    |"/" -> operand1 / operand2
+    |_ -> failwith "Invalid Operation"
+
+
+
+
+
+
+let result = performCalculation()
+
+printfn "Result = %d" result
+
+//using else if statements
+let ifelseCalc () =
+    let operation = readOperation()
+
+    printfn "Enter first Operand"
+    let operand1 = readOperand()
+    printfn "Enter Second Operand"
+    let operand2 = readOperand()
+
+    printf "Calculating %d" operand1
+    printf " %s"operation 
+    printfn " %d"operand2
+    
+
+    if operation = "+" then
+        operand1 + operand2
+    elif operation = "-"then
+        operand1 - operand2
+    elif operation = "*" then
+        operand1 * operand2
+    elif operation = "/" then
+        operand1 / operand2
+    else
+        failwith "Invalid Operation"
+
+
+let r = ifelseCalc()
+printfn "Result = %d" r 
---
+++ example.fsx
@@ -0,0 +1,34 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let checkYear givenYear =
+    if (givenYear % 4 = 0 && givenYear % 100 <> 0) then
+        printfn "This is a leap year"
+    elif(givenYear % 400 = 0) then
+        printfn "This is a leapyear"
+    else
+        printfn "This is not a leap year"
+
+checkYear 2023
+
+///USING MATCH
+
+
+let leapMatch givenYear =
+    match givenYear % 4, givenYear % 100,givenYear % 400 with
+    | 0,_,_ when givenYear % 100 <> 0 ->true
+    | 0,0,0 -> true
+    | _-> false
+
+let result1 = leapMatch 2023
+let result2 = leapMatch 2018
+
+if result1 = true then
+    printfn "Is a leap year"
+else 
+    printfn "not a leap year"
+
+if result2 = true then
+    printfn "Is a leap year"
+else 
+    printfn "not a leap year"
